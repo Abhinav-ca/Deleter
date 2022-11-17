7133eb6fbc596b513d86a9af9b23139d8966c5e9
Abhinav Chadha <Abhinav22221@outlook.com>
2022-11-17 18:20:23 +0530

    updated the code to also allow command line agruments and also updated documentations

--- delet.py
+++ delet.py
@@ -1,28 +1,47 @@
 #!/usr/bin/env python3
-
-
 import os
 import shutil
 import time
-from winreg import DeleteKey
-def dif(folders,show=False,na=[],returns=True):
+import sys
+
+def dif(folders=[],show=False,na=[],returns=True,argv=False,argv_n=[]):
     """
     deletes all the files and folders inside the folders
     parameters:
+    folders: enter the list of folders or a single folder string
     show : shows the files/folders deleted
     na: enter files not to be deleted
     returns: also returns the names of files and folders deleted
+    argv: if argv is True takes the folders from the sys.argv
+    argv_n: you can optionally enter the index of the folder in sys.argv
     """
+    if folders==[] and argv==False:
+       raise TypeError('list of folders is required')
     folder=[]
+    if argv==True:
+        if argv_n==[]:
+            folder=sys.argv[1:]
+        elif type(argv_n)!=list:
+            raise AttributeError('argv_n must be a list')
+        elif len(argv_n)>2 or len(argv_n)<0:
+            raise AttributeError('lenghth of argv_n must be bigger then 0 and smaller than 2')
+        elif not int(argv_n[0])<0:
+            raise AttributeError(' first alue of argv_n must be bigger than zero')
+        else:
+            if len(argv_n)==2:
+                folder=sys.argv[int(argv_n[0]):int(argv_n[1])]
+            elif len(argv_n)==1:
+                folder=sys.argv[int(argv_n[0]):]
+
     if type(folders)==str:
         folder.append(folders)
-    else:
-        folder=folders
     if returns==True:
         tlen=0
         tdel=0
     if returns==True and show==True:
         deleted={'file':[],'folders':[]}
+    elif type(folder)==list:
+        folder+=folders
     for path in folder:
         try: 
             os.chdir(path)
@@ -57,10 +76,13 @@ def dif(folders,show=False,na=[],returns=True):
          print('files deleted : {} & folders deleted : {} '.format(tuple(deleted['file']),tuple(deleted['folders'])))
         
 def main():
-        try:
-            dif(("C:/Windows/Temp","C:/Users/Admin/AppData/Local/Temp"),show=True)
-        except:
-            dif(("/mnt/c/Windows/Temp","/mnt/c/Users/Admin/AppData/Local/Temp"))
+        if len(sys.argv)==1:
+            try:
+                dif(("C:/Windows/Temp","C:/Users/Admin/AppData/Local/Temp"),show=True)
+            except:
+                dif(("/mnt/c/Windows/Temp","/mnt/c/Users/Admin/AppData/Local/Temp"))
+        else:
+                dif(show=True,argv=True)
         time.sleep(1)
 
 main()
